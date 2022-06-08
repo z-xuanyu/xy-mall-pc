de0c258c346d9bfbee9a744dd90510fa4bb1b3a5
阿宇 <969718197@qq.com>
2022-06-08 17:58:24 +0800

    feature(home): 添加首页分类卡片列表组件

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
  * @email: 969718197@qq.com
  * @github: https://github.com/z-xuanyu
  * @Date: 2022-06-01 12:05:31
- * @LastEditTime: 2022-06-08 15:14:22
+ * @LastEditTime: 2022-06-08 17:55:49
  * @Description: Modify here please
 -->
 <script lang="ts">
@@ -16,6 +16,7 @@ export default {
 <script setup lang="ts">
 import HomeBanner from '../components/index/HomeBanner.vue';
 import SeckillCard from '../components/index/SeckillCard.vue';
+import CategoryListCard from '../components/index/CategoryListCard.vue';
 </script>
 
 <template>
@@ -24,6 +25,23 @@ import SeckillCard from '../components/index/SeckillCard.vue';
     <HomeBanner></HomeBanner>
     <!-- 秒杀 -->
     <SeckillCard></SeckillCard>
+    <!-- 分类卡片列表 -->
+    <CategoryListCard
+      title="官方商城"
+      cover="https://demo26.crmeb.net/uploads/attach/2021/11/20211116/824a04a0e24bcb86d13e2a8ddc7d84dd.png"
+    ></CategoryListCard>
+    <CategoryListCard
+      cover="http://demo26.crmeb.net/uploads/attach/2020/10/20201013/d2a316f8e8dd3b8ba4335c8197fb4ff2.png"
+      title="电子产品"
+    ></CategoryListCard>
+    <CategoryListCard
+      cover="http://demo26.crmeb.net/uploads/attach/2020/10/20201013/ac3afff89b11579477fec875f215422a.png"
+      title="家电"
+    ></CategoryListCard>
+    <CategoryListCard
+      cover="http://demo26.crmeb.net/uploads/attach/2020/10/20201013/9558cf872c5d357db73afc7f2c3b9f77.png"
+      title="家居软装"
+    ></CategoryListCard>
   </div>
 </template>
 
