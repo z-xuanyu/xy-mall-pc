d41ce4bb33d381baabbef419326cf08355159c75
阿宇 <969718197@qq.com>
2022-06-09 10:45:55 +0800

    feature(layout): 添加footer组件

--- layouts/components/TheFooter.vue
+++ layouts/components/TheFooter.vue
@@ -4,13 +4,54 @@
  * @email: 969718197@qq.com
  * @github: https://github.com/z-xuanyu
  * @Date: 2022-06-01 16:44:52
- * @LastEditTime: 2022-06-01 16:45:58
+ * @LastEditTime: 2022-06-09 10:44:19
  * @Description: Modify here please
 -->
 <script setup lang="ts"></script>
 
 <template>
-  <div class="">底部</div>
+  <div class="bg-[#f2f2f2] mt-12">
+    <div class="w-[1200px] mx-auto">
+      <ul class="flex justify-around py-10 border-b">
+        <li class="flex items-center">
+          <div
+            class="flex items-center justify-center w-10 h-10 border border-gray-500 rounded-full"
+          >
+            <i class="text-xl text-gray-500 iconfont icon-pinzhong"></i>
+          </div>
+          <p class="ml-3">品种齐全，购物轻松</p>
+        </li>
+        <li class="flex items-center">
+          <div
+            class="flex items-center justify-center w-10 h-10 border border-gray-500 rounded-full"
+          >
+            <i class="text-xl text-gray-500 iconfont icon-zhifa"></i>
+          </div>
+          <p class="ml-3">多仓直发，极速配送</p>
+        </li>
+        <li class="flex items-center">
+          <div
+            class="flex items-center justify-center w-10 h-10 border border-gray-500 rounded-full"
+          >
+            <i class="text-xl text-gray-500 iconfont icon-hanghuo"></i>
+          </div>
+          <p class="ml-3">正品行货，精致服务</p>
+        </li>
+        <li class="flex items-center">
+          <div
+            class="flex items-center justify-center w-10 h-10 border border-gray-500 rounded-full"
+          >
+            <i class="text-xl text-gray-500 iconfont icon-dijia"></i>
+          </div>
+          <p class="ml-3">天天低价，畅选无忧</p>
+        </li>
+      </ul>
+      <div class="py-10 space-y-2 text-sm text-center">
+        <p>阿宇微信：969718197 博客地址：https://www.zhouxuanyu.com</p>
+        <p>Copyright © 2013-2020 ICP备14011498号-3</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
